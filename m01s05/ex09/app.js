5a3c02122b23cce67284e9c2f4a8384b2e85a15c
AndreeaKiara <69343737+AndreeaKiara@users.noreply.github.com>
2023-09-26 21:56:01 +0300

    curs m01s05-06

---
+++ m01s05/ex09/app.js
@@ -0,0 +1,11 @@
+'valid';
+('valid');
+true;
+({ name: 'Andreea' });
+['it', 'will'];
+(function () {});
+(function () {
+  const name = 'Andreea';
+
+  console.log(`Ma cheama ${name}`);
+})('Andreea');
