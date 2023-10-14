ef796c2c2441641f665cedd44988767a35a320d1
AndreeaKiara <69343737+AndreeaKiara@users.noreply.github.com>
2023-10-14 14:48:47 +0300

    m02s01ex01-04 class

---
+++ m02s01/ex03/app.js
@@ -0,0 +1,33 @@
+const Car = {
+  make: '',
+  color: '',
+  wheels: 0,
+  speed: 0,
+  topSpeed: 160,
+  topReverseSpeed: -50,
+
+  displaySpeed: function () {
+    console.log(`Viteza curenta este: ${this.speed}.`);
+  },
+
+  accelerate: function () {
+    this.speed++;
+
+    this.displaySpeed();
+  },
+
+  decelerate: function () {
+    this.speed--;
+
+    this.displaySpeed();
+  },
+};
+
+const audi = Object.create(Car);
+audi.make = 'Audi';
+audi.color = 'black';
+audi.wheels = 4;
+audi.speed = 0;
+audi.topSpeed = 220;
+
+console.log(audi);
