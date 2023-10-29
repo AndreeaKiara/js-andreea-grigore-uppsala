7fde67bf6ae2b48b42194da916d786d7f3af0052
AndreeaKiara <69343737+AndreeaKiara@users.noreply.github.com>
2023-10-29 09:42:06 +0200

    class update m02

--- m02s03/ex07/app.js
+++ m02s03/ex07/app.js
@@ -1,7 +1,17 @@
 const button = document.querySelector('.btn');
+const breakpoint = 650;
 
+document.addEventListener('DOMContentLoaded', function () {
+  const currentWidth = window.innerWidth;
+
+  if (currentWidth >= breakpoint) {
+    button.addEventListener('click', onClick);
+  }
+});
+
+// function functions are hoisted
 function onClick() {
-  alert('butonul a fost apasat');
+  alert('butonul a fost apsat');
 }
 
 button.addEventListener('click', onClick);
@@ -10,7 +20,7 @@ button.addEventListener('click', onClick);
 window.addEventListener('resize', function () {
   const currentWidth = window.innerWidth;
 
-  if (currentWidth < 650) {
+  if (currentWidth < breakpoint) {
     button.removeEventListener('click', onClick);
   } else {
     button.addEventListener('click', onClick);
